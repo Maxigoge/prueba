72cccee24acf080b6d7f7c3de23f29966b3fdff6
Maxigoge <kraizyn@gmail.com>
2021-06-29 02:41:20 -0300

    agregado segunda linea

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Aplicacion de React</h1>
+        <h2>Implementado cambios a futuro</h2>
         <a
           className="App-link"
           href="https://reactjs.org"

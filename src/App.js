934ac0eae03fbbd58f0657547a831c9f52ccd9ff
Maxigoge <kraizyn@gmail.com>
2021-06-29 02:36:02 -0300

    cambie nombre

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Mi app de prueba cheraa</h1>
+        <h1>Aplicacion de React</h1>
         <a
           className="App-link"
           href="https://reactjs.org"

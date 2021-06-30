9b5bf42129913c558d1cc9eeb9491610a5dc8e82
Maxigoge <kraizyn@gmail.com>
2021-06-30 17:30:37 -0300

    agregado el mail para notificacion 3

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React con nosotros!
+          ¡Aprende React con nosotros!
         </a>
       </header>
     </div>

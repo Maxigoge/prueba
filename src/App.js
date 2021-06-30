d7e47cb15677b7f648397224a6539faed6bc18e8
Maxigoge <kraizyn@gmail.com>
2021-06-30 02:35:22 -0300

    slack posta

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Parcial 3 - IdS - 2021</h1>
-        <h2>Implementando cambios a futuro</h2>
+        <h2>¡Implementando cambios a futuro!</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
